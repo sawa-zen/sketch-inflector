5a628a85e487cccc9b2ba9fd83338d3dc9e60fe6
SAWADA Takayoshi <sawadasuiren@gmail.com>
2018-06-04 00:56:45 +0900

    Add upper-case-first package

--- src/upcase_first.js
+++ src/upcase_first.js
@@ -1,7 +1,8 @@
 import UI from 'sketch/ui';
+import upperCaseFirst from 'upper-case-first';
 
 const upcaseFirst = () => {
-  UI.message('Hello world!');
+  UI.message(upperCaseFirst('hello world!'));
 };
 
 export default upcaseFirst;
