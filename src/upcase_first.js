06ea552ffddfe367d1acc86ca8fb1ac237da7c2e
SAWADA Takayoshi <sawadasuiren@gmail.com>
2018-06-04 02:02:45 +0900

    Implement UpperCamel

--- src/upcase_first.js
+++ src/upcase_first.js
@@ -1,8 +1,25 @@
+import { getSelectedDocument } from 'sketch/dom';
 import UI from 'sketch/ui';
 import upperCaseFirst from 'upper-case-first';
 
-const upcaseFirst = () => {
-  UI.message(upperCaseFirst('hello world!'));
+const upcaseFirst = context => {
+  const document = getSelectedDocument();
+
+  if (!document) {
+    return;
+  }
+
+  const symbols = document.getSymbols();
+  symbols.forEach(symbol => {
+    // スラッシュで区切る
+    const words = symbol.name.split('/');
+    const newWords = words.map(word => {
+      return upperCaseFirst(word);
+    });
+    symbol.name = newWords.join('/');
+  });
+
+  UI.message('Finished!');
 };
 
 export default upcaseFirst;
